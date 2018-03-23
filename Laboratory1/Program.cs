b77386a923e81db928dc58176d83002f9c77b0a2
a-Zszywacz <Teresher935@GMail.com>
2018-03-23 17:18:41 +0100

    Pierwszy commit

---
+++ Laboratory1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Plik główny programu zawierający w sobie metodę Main.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laboratory1 {
+    class Program {
+        static void Main(string[] args) {
+			Console.BufferHeight = 1000;
+        }
+    }
+}
---
+++ Laboratory1/SudokuState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory1
+{
+	public class SudokuState : State
+	{
+		public const int SMALL_GRID_SIZE = 3;
+		public const int GRID_SIZE = SMALL_GRID_SIZE * SMALL_GRID_SIZE;
+		private int[,] table;
+		public int[,] Table {
+			get { return this.table; }
+			set { this.table = value; } 11 } 12 13 // reszta implementacj
+
+
+		private string id;
+
+
+		public override string ID
+		{
+			get { return this.id; }
+		}
+
+		public override double ComputeHeuristicGrade()
+		{
+			 throw new NotImplementedException();
+		 }
+		public void Print() {
+
+		}
+
+	}
+}
